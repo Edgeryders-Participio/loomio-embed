9254de7047206f26618bb92758135cf253c85398
Jim Sagevid <jim@diskett.io>
2019-03-26 16:10:13 +0100

    Added some typesss.

--- src/Main.elm
+++ src/Main.elm
@@ -2,17 +2,24 @@ module Main exposing (..)
 
 import Browser
 import Html exposing (Html, text)
+import Http
+
+import Loomio
 
 
 type Msg
-    = NoOp
+    = SetDiscussion Model
+    | NoOp
 
-
-type alias Model =
-    {}
+type alias Model = Loomio.Discussion
 
 init : flags -> (Model, Cmd msg)
-init flags = ({}, Cmd.none)
+init flags =
+    ({}
+    , Http.get
+        { url = "jkhasd"
+        , expect = Http.expectJson SetDiscussion Loomio.decodeDiscussion
+        })
 
 view : Model -> Html Msg
 view model =
@@ -22,6 +29,9 @@ view model =
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
+        SetDiscussion newModel ->
+            ( newModel, Cmd.none )
+
         NoOp ->
             ( model, Cmd.none )
 
