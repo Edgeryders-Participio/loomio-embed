866eea2f94e9970636a4f835c77e1967c04e4599
Jim Sagevid <jim@diskett.io>
2019-03-27 10:41:31 +0100

    Discussion info load.

--- src/Main.elm
+++ src/Main.elm
@@ -4,38 +4,61 @@ import Browser
 import Html exposing (Html, text)
 import Http
 
+import Json.Decode as Json
+
+import Url
+
 import Loomio
 
 
 type Msg
     = SetComments Model
-    | GetComments Model Discussion
+    | GotDiscussion (Result Http.Error Loomio.DiscussionInfo)
     | NoOp
 
 type alias Model = List Loomio.Comment
 
-init : flags -> (Model, Cmd msg)
+discussionUrl : String -> Url.Url
+
+
+init : flags -> (Model, Cmd Msg)
 init flags =
-    ({}
+    ([]
     , Http.get
         { url = "https://talk.theborderland.se/api/v1/discussions/6HMxK2ve" -- from element
-        , expect = Http.expectJson GetComments Loomio.decodeDiscussion
+        , expect =
+            Http.expectJson
+                GotDiscussion
+                ( Json.field "discussions"
+                <| Json.index 0
+                <| Loomio.decodeDiscussion
+                )
         })
 
 
 view : Model -> Html Msg
 view model =
-    text "asd"
+    text (Debug.toString model)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        GetComments discussion ->
-            (model, Http.get
-                { url = "https://talk.theborderland.se/api/v1/events?from=-10&per=20&order=sequence_id&discussion_id=965"
-                , expect = Http.expectJson SetComments Loomio.decodeComments
-                })
+        GotDiscussion result ->
+            case result of 
+                Ok discussionInfo ->
+                    let
+                        _ = Debug.log "discussion" discussionInfo
+                    in
+                        ( model
+                        , Cmd.none )
+                _ ->
+                    ( model, Cmd.none )
+    
+            -- (model, Http.get
+            --     { url = "https://talk.theborderland.se/api/v1/events?from=-10&per=20&order=sequence_id&discussion_id=965"
+            --     , expect = Http.expectJson SetComments Loomio.decodeComments
+            --     })
         SetComments newModel ->
             ( newModel, Cmd.none )
 
